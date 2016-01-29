0c123493e2e25f5378683ce64ba703149f5ab48e
archang <archang@canaan-creative.com>
2016-01-29 11:24:51 +0800

    server: Install ams.conf to etc during seting up

--- server/setup.py
+++ server/setup.py
@@ -1,15 +1,45 @@
 #!/usr/bin/env python3
 
+import os
+import shutil
+import errno
+
 from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+
 import ams
 
-setup(
-    name="ams",
-    version=ams.__version__,
-    packages=find_packages(),
-    scripts=['amscli'],
-    install_requires=["mysql-connector-python >= 2.0"],
-    dependency_links=["http://cdn.mysql.com/Downloads/Connector-Python/"
-                      "mysql-connector-python-2.0.4.zip#"
-                      "md5=3df394d89300db95163f17c843ef49df"],
-)
+
+class install(_install):
+    def run(self):
+        _install.run(self)
+        virtual = os.environ.get('VIRTUAL_ENV')
+        if virtual is not None:
+            directory = os.path.join(virtual, 'etc')
+            try:
+                os.makedirs(directory)
+            except OSError as exception:
+                if exception.errno != errno.EEXIST:
+                    raise
+        else:
+            directory = '/etc'
+
+        dist = os.path.join(directory, 'ams.conf')
+        shutil.copyfile('ams.conf.example', dist)
+        print('copying {} -> {}'.format('ams.conf.example', dist))
+
+
+if __name__ == '__main__':
+    setup(
+        cmdclass={'install': install},
+        name="ams",
+        version=ams.__version__,
+        packages=find_packages(),
+        scripts=['amscli'],
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=["mysql-connector-python >= 2.0"],
+        dependency_links=["http://cdn.mysql.com/Downloads/Connector-Python/"
+                          "mysql-connector-python-2.0.4.zip#"
+                          "md5=3df394d89300db95163f17c843ef49df"],
+    )
